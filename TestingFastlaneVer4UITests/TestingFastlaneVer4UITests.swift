3ed88e8eb2515f151f9192deecc239da6aeae048
Thien Vu Le <vule@gumiviet.com>
2021-04-01 16:58:47 +0700

    feat: first init

--- TestingFastlaneVer4UITests/TestingFastlaneVer4UITests.swift
+++ TestingFastlaneVer4UITests/TestingFastlaneVer4UITests.swift
@@ -8,6 +8,13 @@
 import XCTest
 
 class TestingFastlaneVer4UITests: XCTestCase {
+    
+    override class func setUp() {
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
+        snapshot("1stTestView")
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
